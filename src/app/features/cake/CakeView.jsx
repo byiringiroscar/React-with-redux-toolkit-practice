2df0cfbddcbf70d59c05d1c42e80a0904104b9c8
byiringiroscar <byiringoroscar@gmail.com>
2023-04-13 22:57:58 +0200

    Use of dispatch to access reducer cake

--- src/app/features/cake/CakeView.jsx
+++ src/app/features/cake/CakeView.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { ordered, restocked } from './cakeSlice';
 
 const CakeView = () => {
 const numOfCakes = useSelector((state) => state.cake.numOfCakes)
+const dispatch =  useDispatch()
   return (
     <div>
         <h2>Number of cakes - {numOfCakes}</h2>
-        <button>Order cake</button>
-        <button>Restock cakes</button>
+        <button onClick={() => dispatch(ordered())}>Order cake</button>
+        <button onClick={() => dispatch(restocked(5))}> Restock cakes</button>
     </div>
   )
 }
