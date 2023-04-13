4dd917d915af8170d17b598773fa68924f23b8dc
byiringiroscar <byiringoroscar@gmail.com>
2023-04-13 22:54:16 +0200

    Read useSelector to get data from redux store and initial state

--- src/app/features/cake/CakeView.jsx
+++ src/app/features/cake/CakeView.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 const CakeView = () => {
+const numOfCakes = useSelector((state) => state.cake.numOfCakes)
   return (
     <div>
-        <h2>Number of cakes - </h2>
+        <h2>Number of cakes - {numOfCakes}</h2>
         <button>Order cake</button>
         <button>Restock cakes</button>
     </div>
--- src/app/features/icecream/icecreamView.jsx
+++ src/app/features/icecream/icecreamView.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
-const icecreamView = () => {
+const IcecreamView = () => {
+const numOfIceCream = useSelector((state) => state.icecream.numOfIceCream)
   return (
     <div>
-        <h2>Number of ice creams - </h2>
+        <h2>Number of ice creams - {numOfIceCream}</h2>
         <button>Order ice cream</button>
         <button>Restock  ice creams</button>
     </div>
   )
 }
 
-export default icecreamView
+export default IcecreamView
