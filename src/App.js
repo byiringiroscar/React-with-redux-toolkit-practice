63dcaf9e9f80902a7795b03b3624aaf1872b31c8
byiringiroscar <byiringoroscar@gmail.com>
2023-04-13 21:43:31 +0200

    Update layout for connecting redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+
+import CakeView from './app/features/cake/CakeView'
+import  IcecreamView  from './app/features/icecream/icecreamView'
+import  UserView  from './app/features/user/userView'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CakeView />
+      <IcecreamView />
+      <UserView />
     </div>
   );
 }
---
+++ src/app/features/cake/CakeView.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CakeView = () => {
+  return (
+    <div>
+        <h2>Number of cakes - </h2>
+        <button>Order cake</button>
+        <button>Restock cakes</button>
+    </div>
+  )
+}
+
+export default CakeView
