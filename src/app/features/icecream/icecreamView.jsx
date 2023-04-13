38f3c3230f00e4a3e8adee49ad4a2946d131a3d8
byiringiroscar <byiringoroscar@gmail.com>
2023-04-13 23:09:27 +0200

    Add dynamic restore by input from user with dispatch

--- src/app/features/icecream/icecreamView.jsx
+++ src/app/features/icecream/icecreamView.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { ordered, restocked } from './icecreamSlice'
 
 const IcecreamView = () => {
-const numOfIceCream = useSelector((state) => state.icecream.numOfIceCream)
-  return (
-    <div>
-        <h2>Number of ice creams - {numOfIceCream}</h2>
-        <button>Order ice cream</button>
-        <button>Restock  ice creams</button>
-    </div>
-  )
+    const [value, setValue] = useState(1)
+    const numOfIceCream = useSelector((state) => state.icecream.numOfIceCream)
+    const dispatch = useDispatch()
+    return (
+        <div>
+            <h2>Number of ice creams - {numOfIceCream}</h2>
+            <button onClick={() => dispatch(ordered())}>Order ice cream</button>
+            <input 
+            type='number' 
+            value={value} 
+            onChange={(e) => setValue(parseInt(e.target.value))}/>
+            <button onClick={() => dispatch(restocked(value))}>Restock  ice creams</button>
+        </div>
+    )
 }
 
 export default IcecreamView
